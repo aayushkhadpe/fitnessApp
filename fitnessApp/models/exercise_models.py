481dd995031fb28b118ebb95a2bce28821cb1a47
Aayush Khadpe <aayush.khadpe@gmail.com>
2025-06-01 17:32:24 -0700

    made video optional and created data

--- fitnessApp/models/exercise_models.py
+++ fitnessApp/models/exercise_models.py
@@ -5,7 +5,7 @@ from fitnessApp.choices import *
 class Exercise(models.Model):
     name = models.CharField(max_length=200)
     description = models.CharField(max_length=1000, blank=True)
-    video_id = models.CharField(max_length=50, blank=True)
+    video_id = models.CharField(max_length=50, null=True)
     public_flag = models.BooleanField(default=False)
     mode =  models.CharField(max_length=20, choices=MODE_CHOICES, default="REPS")
     weight_flag = models.BooleanField(default=True)
