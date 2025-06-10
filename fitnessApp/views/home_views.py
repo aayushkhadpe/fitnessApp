63e65bd79de9f1843a2f5e20850e18ad05da79e4
Aayush <aayushkhadpe26@mittymonarch.com>
2025-06-10 23:19:36 +0000

    new home page

--- fitnessApp/views/home_views.py
+++ fitnessApp/views/home_views.py
@@ -11,9 +11,16 @@ class HomeView(TemplateView):
         context = super().get_context_data(**kwargs)
         
         if (self.request.user.is_authenticated):
-            context['activeSessions'] = WorkoutSession.objects.filter(user=self.request.user, scheduled_date=date.today())
-            context['upcomingSessions'] = WorkoutSession.objects.filter(Q(user=self.request.user) & (Q(scheduled_date__gt=date.today()) | Q(scheduled_date__isnull=True))).order_by((F('scheduled_date').asc(nulls_last=True)))
-            context['pastSessions'] = WorkoutSession.objects.filter(user=self.request.user, scheduled_date__lt=date.today()).order_by('-scheduled_date')
-            context['today'] = date.today()
+            if (self.request.user.coach_flag):
+                context['activeSessions'] = WorkoutSession.objects.filter(Q(user=self.request.user) | Q(user__coach=self.request.user), scheduled_date=date.today())
+                context['upcomingSessions'] = WorkoutSession.objects.filter(Q(user=self.request.user) & (Q(scheduled_date__gt=date.today()) | Q(scheduled_date__isnull=True))).order_by((F('scheduled_date').asc(nulls_last=True)))
+                context['pastSessions'] = WorkoutSession.objects.filter(user=self.request.user, scheduled_date__lt=date.today()).order_by('-scheduled_date')
+                context['today'] = date.today()
+
+            else: 
+                context['activeSessions'] = WorkoutSession.objects.filter(user=self.request.user, scheduled_date=date.today())
+                context['upcomingSessions'] = WorkoutSession.objects.filter(Q(user=self.request.user) & (Q(scheduled_date__gt=date.today()) | Q(scheduled_date__isnull=True))).order_by((F('scheduled_date').asc(nulls_last=True)))
+                context['pastSessions'] = WorkoutSession.objects.filter(user=self.request.user, scheduled_date__lt=date.today()).order_by('-scheduled_date')
+                context['today'] = date.today()
 
         return context
