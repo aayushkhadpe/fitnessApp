c99a9543426de83f1f1415aa47d0bc7ff4fed85e
Aayush Khadpe <aayush.khadpe@gmail.com>
2025-07-17 22:02:32 -0700

    fixed session views

--- fitnessApp/views/session_views.py
+++ fitnessApp/views/session_views.py
@@ -1,3 +1,4 @@
+from django.views.generic import ListView
 from django.views.generic import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.db.models import Q, F
@@ -6,47 +7,47 @@ from fitnessApp.models import *
 from fitnessApp.forms import *
 
 
-class SessionView(LoginRequiredMixin, TemplateView):
+class SessionView(LoginRequiredMixin, ListView):
+    model = WorkoutSession
     template_name = "session.html"
+    paginate_by = 50
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+
+        if (self.request.user.person.coach_flag):
+            queryset = queryset.filter(Q(creator=self.request.user.person), scheduled_date=date.today()).order_by((F('scheduled_time').asc(nulls_last=True)))
+        else: 
+            queryset = queryset.filter(person=self.request.user.person, scheduled_date=date.today()).order_by((F('scheduled_time').asc(nulls_last=True)))
+
+        return queryset
    
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        
-        if (self.request.user.is_authenticated):
-            context['today'] = date.today()
-            if (self.request.user.person.coach_flag):
-                context['activeSessions'] = WorkoutSession.objects.filter(Q(creator=self.request.user.person), scheduled_date=date.today()).order_by((F('scheduled_time').asc(nulls_last=True)))
-            else: 
-                context['activeSessions'] = WorkoutSession.objects.filter(person=self.request.user.person, scheduled_date=date.today()).order_by((F('scheduled_time').asc(nulls_last=True)))
-
-        return context
-    
-class UpcomingSessionView(LoginRequiredMixin, TemplateView):
+class UpcomingSessionView(LoginRequiredMixin, ListView):
+    model = WorkoutSession
     template_name = "upcoming_session.html"
+    paginate_by = 50
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        
-        if (self.request.user.is_authenticated):
-            context['today'] = date.today()
-            if (self.request.user.person.coach_flag):
-                context['upcomingSessions'] = WorkoutSession.objects.filter(Q(creator=self.request.user.person) & (Q(scheduled_date__gt=date.today()) | Q(scheduled_date__isnull=True))).order_by((F('scheduled_date').asc(nulls_last=True)), (F('scheduled_time').asc(nulls_last=True)))
-            else: 
-                context['upcomingSessions'] = WorkoutSession.objects.filter(Q(person=self.request.user.person) & (Q(scheduled_date__gt=date.today()) | Q(scheduled_date__isnull=True))).order_by((F('scheduled_date').asc(nulls_last=True)), (F('scheduled_time').asc(nulls_last=True)))
+    def get_queryset(self):
+        queryset = super().get_queryset()
 
-        return context
+        if (self.request.user.person.coach_flag):
+            queryset = queryset.filter(Q(creator=self.request.user.person) & (Q(scheduled_date__gt=date.today()) | Q(scheduled_date__isnull=True))).order_by((F('scheduled_date').asc(nulls_last=True)), (F('scheduled_time').asc(nulls_last=True)))
+        else: 
+            queryset = queryset.filter(Q(person=self.request.user.person) & (Q(scheduled_date__gt=date.today()) | Q(scheduled_date__isnull=True))).order_by((F('scheduled_date').asc(nulls_last=True)), (F('scheduled_time').asc(nulls_last=True)))
 
-class PastSessionView(LoginRequiredMixin, TemplateView):
+        return queryset
+          
+class PastSessionView(LoginRequiredMixin, ListView):
+    model = WorkoutSession
     template_name = "past_session.html"
+    paginate_by = 50
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        
-        if (self.request.user.is_authenticated):
-            context['today'] = date.today()
-            if (self.request.user.person.coach_flag):
-                context['pastSessions'] = WorkoutSession.objects.filter(Q(creator=self.request.user.person) & (Q(scheduled_date__lt=date.today()) | Q(scheduled_date__isnull=True))).order_by((F('scheduled_date').desc(nulls_last=True)), (F('scheduled_time').desc(nulls_last=True)))
-            else: 
-                context['pastSessions'] = WorkoutSession.objects.filter(Q(person=self.request.user.person) & (Q(scheduled_date__lt=date.today()) | Q(scheduled_date__isnull=True))).order_by((F('scheduled_date').desc(nulls_last=True)), (F('scheduled_time').desc(nulls_last=True)))
+    def get_queryset(self):
+        queryset = super().get_queryset()
 
-        return context
+        if (self.request.user.person.coach_flag):
+            queryset = queryset.filter(Q(creator=self.request.user.person) & (Q(scheduled_date__lt=date.today()) | Q(scheduled_date__isnull=True))).order_by((F('scheduled_date').desc(nulls_last=True)), (F('scheduled_time').desc(nulls_last=True)))
+        else: 
+            queryset = queryset.filter(Q(person=self.request.user.person) & (Q(scheduled_date__lt=date.today()) | Q(scheduled_date__isnull=True))).order_by((F('scheduled_date').desc(nulls_last=True)), (F('scheduled_time').desc(nulls_last=True)))
+
+        return queryset
